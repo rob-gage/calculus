dacf3a3705e39323d7a617f4b25a226c5fc32a36
Rob Gage <rmg@oakleycomputing.org>
2025-11-21 20:50:43 -0500

    Add flattening for nested additions and multiplications Add simple reductions for integers

--- core/src/expression.rs
+++ core/src/expression.rs
@@ -1,6 +1,7 @@
 // Copyright Rob Gage 2025
 
 use num_bigint::BigInt;
+use num_integer::Integer;
 
 /// An algebraic expression
 #[derive(Clone, PartialEq)]
@@ -21,9 +22,113 @@ pub enum Expression {
 
 impl Expression {
 
+    /// Flattens a potentially nested `Expression`, or returns it unchanged if not nested
+    fn flatten(self) -> Self {
+        use Expression::*;
+        match self {
+            Addition (terms) => {
+                let mut flattened: Vec<Expression> = Vec::new();
+                for term in terms {
+                    match term {
+                        Addition (addition_terms) => flattened.extend(
+                            addition_terms.into_iter().map(|term| term.flatten())
+                        ),
+                        other => flattened.push(other.flatten()),
+                    }
+                }
+                Addition (flattened)
+            },
+            Subtraction (terms) => Subtraction(Box::new((terms.0.flatten(), terms.1.flatten()))),
+            Multiplication (terms) => {
+                let mut flattened: Vec<Expression> = Vec::new();
+                for term in terms {
+                    match term {
+                        Multiplication (multiplication_terms) => flattened.extend(
+                            multiplication_terms.into_iter().map(|term| term.flatten())
+                        ),
+                        other => flattened.push(other.flatten()),
+                    }
+                }
+                Multiplication (flattened)
+            }
+            Division (terms) => Division(Box::new((
+                terms.0.flatten(),
+                terms.1.flatten(),
+            ))),
+            other => other,
+        }
+    }
+
     /// Reduce an `Expression`, or returns it unchanged if not reducible
     fn reduce(self) -> Self {
-        todo!()
+        use Expression::*;
+        let flattened: Self = self.flatten();
+        match flattened {
+            Addition (terms) => match terms {
+                // remove unnecessary `Addition` from single term
+                terms if terms.len() == 1 => terms[0].clone(),
+                // convert empty `Addition` to `0`
+                terms if terms.len() == 0 => Integer (BigInt::from(0)),
+                // reduce other `Addition`s
+                terms => {
+                    let mut integer_sum: BigInt = BigInt::ZERO;
+                    let mut other_terms: Vec<Expression> = Vec::new();
+                    for term in terms {
+                        match term {
+                            Integer (integer) => integer_sum += integer,
+                            other => other_terms.push(other.clone()),
+                        }
+                    }
+                    if integer_sum == BigInt::ZERO {
+                        if other_terms.is_empty() { return Integer (BigInt::ZERO) }
+                    } else { other_terms.push(Integer (integer_sum)) }
+                    Addition (other_terms)
+                }
+            }
+            Subtraction (terms) => match *terms {
+                // difference is zero
+                (left, right) if left == right => Integer (BigInt::ZERO),
+                // subtract integers
+                (Integer (left), Integer(right)) => Integer (left - right),
+                _ => Subtraction (terms)
+            }
+            Multiplication (factors) => match factors {
+                // remove unnecessary `Multiplication` from single term
+                factors if factors.len() == 1 => factors[0].clone(),
+                // convert empty `Multiplication` to `0`
+                factors if factors.len() == 0 => Integer (BigInt::from(0)),
+                // reduce other `Multiplication`s
+                terms => {
+                    let mut integer_product: BigInt = BigInt::from(1);
+                    let mut other_terms: Vec<Expression> = Vec::new();
+                    for term in terms {
+                        match term {
+                            Integer (integer) => integer_product *= integer,
+                            other => other_terms.push(other.clone()),
+                        }
+                    }
+                    if integer_product == BigInt::ZERO {
+                        return Integer (BigInt::ZERO);
+                    } else if integer_product != BigInt::from(1) {
+                        other_terms.push(Integer (integer_product));
+                    }
+                    Multiplication (other_terms)
+                }
+            }
+            Division (terms) => match *terms {
+                // reduce fractions
+                (Integer (numerator), Integer (denominator)) => {
+                    let gcd: BigInt = numerator.gcd(&denominator);
+                    let numerator: BigInt = numerator / &gcd;
+                    let denominator: BigInt = denominator / &gcd;
+                    if denominator == BigInt::from(1) { Integer (numerator) } else {
+                        Division (Box::new((Integer (numerator), Integer (denominator))))
+                    }
+                }
+                _ => Division (terms)
+            }
+            other => other
+        }
     }
 
     /// Differentiates this `Expression` with respect to a variable
@@ -34,33 +139,33 @@ impl Expression {
             Variable (name) if name == variable => Integer (BigInt::from(1)),
             Variable (_) => Integer (BigInt::from(0)),
             Integer (_) => Integer (BigInt::from(0)),
-            Addition (operands) => Addition (operands.iter()
+            Addition (terms) => Addition (terms.iter()
                 .map(|operand| operand.differentiate(variable))
                 .collect()
             ),
-            Subtraction (operands) => Subtraction (Box::new((
-                operands.0.differentiate(variable), operands.1.differentiate(variable)
+            Subtraction (terms) => Subtraction (Box::new((
+                terms.0.differentiate(variable), terms.1.differentiate(variable)
             ))),
-            Multiplication (operands) => Addition (operands.iter()
+            Multiplication (factors) => Addition (factors.iter()
                 .enumerate()
                 .map(|(factor_index, factor)| {
-                    let mut output: Vec<Expression> = Vec::with_capacity(operands.len());
+                    let mut output: Vec<Expression> = Vec::with_capacity(factors.len());
                     output.push(factor.differentiate(variable));
-                    for index in 0..operands.len() {
+                    for index in 0..factors.len() {
                         if index != factor_index {
-                            output.push(operands[index].clone());
+                            output.push(factors[index].clone());
                         }
                     }
                     Multiplication (output)
                 })
                 .collect()
             ),
-            Division (operands) => Division (Box::new((
+            Division (terms) => Division (Box::new((
                 Subtraction (Box::new((
-                    Multiplication (vec![operands.0.differentiate(variable), operands.1.clone()]),
-                    Multiplication (vec![operands.0.clone(), operands.1.differentiate(variable)]),
+                    Multiplication (vec![terms.0.differentiate(variable), terms.1.clone()]),
+                    Multiplication (vec![terms.0.clone(), terms.1.differentiate(variable)]),
                 ))),
-                Multiplication (vec![operands.1.clone(), operands.1.clone()])
+                Multiplication (vec![terms.1.clone(), terms.1.clone()])
             )))
         }.reduce()
     }
